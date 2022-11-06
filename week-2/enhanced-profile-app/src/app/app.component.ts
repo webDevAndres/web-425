959b86065e60071858668a8886787f5e46360138
webDevAndres <macias.andres02@gmail.com>
2022-11-06 13:28:46 -0600

    exercise 2.3 - data binding

---
+++ week-2/enhanced-profile-app/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  isLoggedIn: Boolean = true;
+
+ assignment = 'Exercise 2.3 - Data Binding.';
+}
