9ccb2b59ea54e09946a17d277cbbff6313eae9c9
webDevAndres <macias.andres02@gmail.com>
2022-11-06 18:31:19 -0600

    exercise 2.4

--- week-2/composer-app/src/app/composer-list/composer-list.component.ts
+++ week-2/composer-app/src/app/composer-list/composer-list.component.ts
@@ -1,13 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+export class Composer {
+  fullName: string;
+  genre: string;
+
+  constructor(fullName: string, genre: string) {
+    this.fullName = fullName;
+    this.genre = genre;
+  }
+}
 @Component({
   selector: 'app-composer-list',
   templateUrl: './composer-list.component.html',
   styleUrls: ['./composer-list.component.css']
 })
 export class ComposerListComponent implements OnInit {
+  composers: Array<Composer>;
 
-  constructor() { }
+  constructor() {
+    this.composers = [
+      new Composer("Ludwig Van Beethoven", "Classical"),
+      new Composer("Johann Sebastian Bach", "Classical"),
+      new Composer("Wolfgang Amadeus Mozart", "Classical"),
+      new Composer("Antonio Vivaldi", "Classical"),
+      new Composer("Johannes Brahms", "Classical")]
+  }
 
   ngOnInit(): void {
   }
