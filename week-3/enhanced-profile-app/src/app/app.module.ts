b5bb85ad3392f2ca93f265062139ce830293a651
webDevAndres <macias.andres02@gmail.com>
2022-11-06 22:51:02 -0600

    assignment 3.4

---
+++ week-3/enhanced-profile-app/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { MyImageComponent } from './my-image/my-image.component';
+import { MyDetailsComponent } from './my-details/my-details.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutes } from './app.routing';
+import { RouterModule } from '@angular/router';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyImageComponent,
+    MyDetailsComponent,
+    SignInComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(AppRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ week-3/enhanced-profile-app/src/app/my-details/my-details.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+
+export default class Person {
+  fullName: string;
+  favoriteFood: string;
+  favoriteColor: string;
+  keywords = [
+    "#Typescript",
+    "#2020",
+    "#CodingWithAngular",
+    "#ngOmaha"
+  ];
+
+  constructor(fullName: string, favoriteFood: string, favoriteColor: string) {
+    this.fullName = fullName;
+    this.favoriteFood = favoriteFood;
+    this.favoriteColor = favoriteColor;
+  }
+  toString() {
+    console.log(`\n . Full name: ${this.fullName}\n . Favorite Food: ${this.favoriteFood}\n . Favorite Color: ${this.favoriteColor}`);
+  }
+}
+
+@Component({
+  selector: 'app-my-details',
+  templateUrl: './my-details.component.html',
+  styleUrls: ['./my-details.component.css']
+})
+export class MyDetailsComponent implements OnInit {
+  myProfile: Person;
+
+  constructor() {
+    this.myProfile = new Person("Andres Macias", "Pizza", "Purple");
+    this.myProfile.toString();
+   }
+
+
+  ngOnInit(): void {
+  }
+
+}
