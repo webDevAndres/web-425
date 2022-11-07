5253a53472b099b56029a10592537dfadacdab49
webDevAndres <macias.andres02@gmail.com>
2022-11-06 22:47:55 -0600

    exercise 3.3

---
+++ week-3/secure-profile-app/src/app/home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  isLoggedIn: Boolean;
+  constructor(private route: ActivatedRoute) {
+    this.isLoggedIn = Boolean(this.route.snapshot.queryParamMap.get('isLoggedIn'));
+  }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ week-3/secure-profile-app/src/app/sign-in/sign-in.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-sign-in',
+  templateUrl: './sign-in.component.html',
+  styleUrls: ['./sign-in.component.css']
+})
+export class SignInComponent implements OnInit {
+  isLoggedIn = false;
+
+  constructor(private router: Router) {
+
+
+   }
+   signin() {
+      this.isLoggedIn = true;
+      this.router.navigate(['/home'], {queryParams: {isLoggedIn: this.isLoggedIn}, skipLocationChange:true});
+  }
+  ngOnInit(): void {
+  }
+
+}
