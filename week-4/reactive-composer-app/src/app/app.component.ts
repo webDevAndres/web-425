b5f253ec3591c1269472d6f35b9638c954be2f36
webDevAndres <macias.andres02@gmail.com>
2022-11-13 23:08:54 -0600

    exercise 4.3

---
+++ week-4/reactive-composer-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  assignment= 'Exercise 4.3 - Handling Events with Observables';
+}
