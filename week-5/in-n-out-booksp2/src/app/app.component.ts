2b017dda621a71ccb047dd3653f795fb134b52b5
webDevAndres <macias.andres02@gmail.com>
2022-11-20 22:02:25 -0600

    exercise 5.3 - Data Tables

---
+++ week-5/in-n-out-booksp2/src/app/app.component.ts
@@ -0,0 +1,21 @@
+/**
+ * Title: app.module.ts
+ * Author: Andres Macias
+ * Date: 11/20/22
+ * Description: App module
+ */
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  assignment: string;
+
+  constructor() {
+    this.assignment = "Exercise 5.3 - Data Tables";
+  }
+}
---
+++ week-5/in-n-out-booksp2/src/app/book-list/book-list.component.ts
@@ -0,0 +1,35 @@
+/**
+ * Title: book-list.component.ts
+ * Author: Andres Macias
+ * Date: 11/20/22
+ * Description: component for the book list
+ */
+
+import { Component, OnInit } from '@angular/core';
+import { BooksService } from '../books.service';
+import { IBook } from '../book.interface';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.css']
+})
+export class BookListComponent implements OnInit {
+
+  books: Observable<IBook[]>;
+  header: Array<string> = ['isbn', 'title', 'numOfPages', 'authors']
+  book: IBook;
+
+  constructor(private booksService: BooksService) {
+    this.books = this.booksService.getBooks();
+  }
+
+  ngOnInit(): void {
+  }
+
+  showBookDetails(isbn: string) {
+    this.book = this.booksService.getBook(isbn);
+    console.log(this.book);
+  }
+}
