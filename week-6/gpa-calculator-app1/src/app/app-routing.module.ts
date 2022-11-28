7766f4e49440a3c8c7fb5f3c2a3c48ce802173fe
webDevAndres <macias.andres02@gmail.com>
2022-11-27 21:36:25 -0600

    Exercise 6.3 - layouts

---
+++ week-6/gpa-calculator-app1/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+/**
+ * Title: app.module.ts
+ * Author: Andres Macias
+ * Date: 11/27/22
+ * Description: Routing module
+ */
+
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { BaseLayoutComponent } from './base-layout/base-layout.component';
+import { HomeComponent } from './home/home.component';
+import { AuthLayoutComponent } from './auth-layout/auth-layout.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+const routes: Routes = [
+  { path: '', component: BaseLayoutComponent, children: [{ path: '', component: HomeComponent }] },
+  { path: 'session', component: AuthLayoutComponent, children: [{ path: 'not-found', component: NotFoundComponent }] },
+  { path: '**', redirectTo: 'session/not-found' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
