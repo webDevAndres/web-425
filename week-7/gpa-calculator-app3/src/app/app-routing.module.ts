dfe50de7142a2849b9aef14385e050f5b2c5a7fd
webDevAndres <macias.andres02@gmail.com>
2022-12-04 22:44:49 -0600

    exercise 7.2

--- week-7/gpa-calculator-app3/src/app/app-routing.module.ts
+++ week-7/gpa-calculator-app3/src/app/app-routing.module.ts
@@ -12,11 +12,38 @@ import { HomeComponent } from './home/home.component';
 import { AuthLayoutComponent } from './auth-layout/auth-layout.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { SignInComponent } from './sign-in/sign-in.component';
+import { SignInGuard } from './sign-in.guard';
 
 const routes: Routes = [
-  { path: '', component: BaseLayoutComponent, children: [{ path: '', component: HomeComponent }] },
-  { path: 'session', component: AuthLayoutComponent, children: [{ path: 'not-found', component: NotFoundComponent },{path: 'sign-in', component: SignInComponent }] },
-  { path: '**', redirectTo: 'session/not-found' }
+  {
+    path: '',
+    component: BaseLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent
+      }
+    ],
+    canActivate: [SignInGuard]
+  },
+  {
+    path: 'session',
+    component: AuthLayoutComponent,
+    children: [
+      {
+        path: 'not-found',
+        component: NotFoundComponent
+      },
+      {
+        path: 'sign-in',
+        component: SignInComponent
+      }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: 'session/not-found'
+  }
 ];
 
 @NgModule({
