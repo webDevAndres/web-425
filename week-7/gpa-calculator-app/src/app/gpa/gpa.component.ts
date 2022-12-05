2d35acb8434cad83937abd9d2a946eb89b28a0d8
webDevAndres <macias.andres02@gmail.com>
2022-12-04 23:01:21 -0600

    gpa calculator

---
+++ week-7/gpa-calculator-app/src/app/gpa/gpa.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-gpa',
+  templateUrl: './gpa.component.html',
+  styleUrls: ['./gpa.component.css']
+})
+export class GpaComponent implements OnInit {
+  @Input() gpaTotal: number;
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
